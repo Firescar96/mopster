8f0b33eee71c04481a8941a6b2fba659921d39b3
Firescar96 <nchinda2@gmail.com>
2016-06-12 03:48:30 -0400

    Change default to missioncontrol url:port

--- app/routes/authorized.js
+++ app/routes/authorized.js
@@ -2,16 +2,12 @@ import Ember from "ember";
 
 export default Ember.Route.extend({
   beforeModel: function () {
-    const serverHost = localStorage.getItem("serverHost");
-    const serverPort = localStorage.getItem("serverPort");
-    if (serverHost && serverPort) {
-      this.controllerFor("application").set("bare", false);
-      this.get("mop").setProperties({
-        serverHost: serverHost,
-        serverPort: serverPort,
-      });
-    } else {
-      this.transitionTo("setup");
-    }
+    const serverHost = "18.239.0.62";
+    const serverPort = "6680";
+    this.controllerFor("application").set("bare", false);
+    this.get("mop").setProperties({
+      serverHost: serverHost,
+      serverPort: serverPort,
+    });
   },
 });
